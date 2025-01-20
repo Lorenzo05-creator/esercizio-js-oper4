1dc94810aad20ae75bcd5a4be7826746b6f91476
Lorenzo05-creator <lory4jz@gmail.com>
2025-01-20 14:42:42 +0100

    esercizio-operatori4.js

--- esercizio-operatori4.js
+++ esercizio-operatori4.js
@@ -6,6 +6,6 @@ let gattiInFila = 6;
 
 let fileTotali = Math.round(totaleGatti / gattiInFila);
 let gattiFuori = totaleGatti % gattiInFila;
-let gattiMancanti = 7 - gattiFuori
+let gattiMancanti = gattiInFila - gattiFuori
 
 console.log(`Ci sono ${fileTotali} file di gatti e ne mancano ${gattiMancanti} per una nuova fila, con un avanzo di ${gattiFuori}`);
